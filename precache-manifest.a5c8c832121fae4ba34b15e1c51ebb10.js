8983575a8d8a819b720878a73304ed1cd36f4d5a
stank2010 <stankkpp_hualaem@hotmail.com>
2020-01-14 20:02:59 +0700

    1st

---
+++ precache-manifest.a5c8c832121fae4ba34b15e1c51ebb10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1242b542975fb06e84b1e48eb0989e42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7896f33661cd1076f19",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b24129ec4267527ca248",
+    "url": "/static/css/main.b4432e62.chunk.css"
+  },
+  {
+    "revision": "c7896f33661cd1076f19",
+    "url": "/static/js/2.cec2b2cc.chunk.js"
+  },
+  {
+    "revision": "07a587ebd1f4bf591554f86c2dfc8f16",
+    "url": "/static/js/2.cec2b2cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b24129ec4267527ca248",
+    "url": "/static/js/main.49e1c100.chunk.js"
+  },
+  {
+    "revision": "755e79db65c34156829f",
+    "url": "/static/js/runtime-main.2ea539ab.js"
+  },
+  {
+    "revision": "fbfb528a1ff8be028aa0e66c024347c6",
+    "url": "/static/media/Capture.fbfb528a.png"
+  }
+]);
